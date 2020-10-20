677948b229521271f5347b44120f15b2423cad78
Dan Grover <dangrover@LT-A01-021.local>
2020-10-20 17:29:03 +0100

    Made loads of changes to the logging

--- api/api.py
+++ api/api.py
@@ -1,94 +1,191 @@
 import threading
 import time
-import os
 import logging
 
-import pigpio
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from humanfriendly import format_timespan
-import board
-import busio
-import adafruit_ssd1306 as af
-# import digitalio
-from PIL import Image, ImageDraw, ImageFont
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-i_log = os.path.join(basedir, "logs", "api_info.log")
-logging.basicConfig(filename=i_log, level=20)
+from pi import PiDisplay, PiGPIO
 
 
-class PiDisplay():
-    def __init__(self):
-        i2c = busio.I2C(board.SCL, board.SDA)
-        self.oled = af.SSD1306_I2C(128, 64, i2c)
-        self.oled.fill(0)
-        self.oled.show()
-        self.image = Image.new("1", (self.oled.width, self.oled.height))
-        draw = ImageDraw.Draw(self.image)
-        draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
-        draw.rectangle((5, 5, self.oled.width - 5 - 1, self.oled.height - 5 - 1),
-                       outline=255,
-                       fill=255)
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        rob = os.path.join(basedir, "Roboto-Medium.ttf")
-        try:
-            self.font = ImageFont.truetype(rob)
-            logging.info("Successfully loaded Roboto-Medium Font.")
-        except OSError:
-            logging.info("Error loading Roboto-Medium - falling back to load_default()")
-            self.font = ImageFont.load_default()
-        text = "Hey, fucko!"
-        (font_width, font_height) = self.font.getsize(text)
-        draw.text(
-            (self.oled.width // 2 - font_width // 2, self.oled.height // 2 - font_height // 2),
-            text,
-            font=self.font,
-            fill=0,
-        )
-
-        # Display image
-        self.oled.image(self.image)
-        self.oled.show()
-
-    def writeText(self, t):
-        image = Image.new("1", (self.oled.width, self.oled.height))
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
-        draw.rectangle((5, 5, self.oled.width - 5 - 1, self.oled.height - 5 - 1),
-                       outline=255,
-                       fill=255)
-        (font_width, font_height) = self.font.getsize(t)
-        draw.text(
-            (self.oled.width // 2 - font_width // 2, self.oled.height // 2 - font_height // 2),
-            t,
-            font=self.font,
-            fill=0,
-        )
-        self.oled.image(image)
-        self.oled.show()
-
-    def screenOff(self):
-        image = Image.new("1", (self.oled.width, self.oled.height))
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
-        self.oled.image(image)
-        self.oled.show()
+logging.getLogger(__name__)
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'shedapi'
 lightsOn = False
 fanTimer = 0
-pi3 = pigpio.pi(host='pi3.local')
+pause = 0
 PiD = PiDisplay()
+PiIO = PiGPIO()
+
+
+# Route functions
+
+
+@app.route('/', methods=['GET'])
+def apiIndex():
+    # Returns standard data of variable values.
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    data = standardData()
+    return jsonify(data)
+
+
+@app.route('/add/<obj>/<int:val>', methods=['POST'])
+def apiAdd(obj, val):
+    # Adds time to the fan counter, ignores the lights.
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    logging.info("api.apiAdd Function: obj: {}, val: {}".format(obj, val))
+    global fanTimer
+    global lightsOn
+    data = standardData()
+    if val > 0:
+        if obj == "fan":
+            switchFan(True, t=val)
+            message = "{} added to the fan timer.".format(format_timespan(int(val)))
+            logging.info(message)
+            data.update({
+                "fanTimer": fanTimer,
+                "message": message
+            })
+        if obj == "lights":
+            data["message"] = "Lights have no timer function."
+            data["success"] = False
+    else:
+        data["message"] = "0s selected - nothing to do."
+        data["success"] = False
+    return jsonify(data)
+
+
+@app.route('/switch/<obj>', methods=['POST'])
+def apiSwitchDefault(obj):
+    # Switches the lights or fan either on or off - whichever it wasn't previously.
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    logging.info("api.apiSwitchDefault Function: obj: {}".format(obj))
+    s = 0
+    if obj == "lights":
+        global lightsOn
+        s = int(not lightsOn)
+    if obj == "fan":
+        logging.info("Switching fan.")
+        global fanTimer
+        logging.info("fanTimer Value: {}".format(fanTimer))
+        if fanTimer > 1:
+            logging.info("Greater than one, so switching 's' to 0.")
+            s = 0
+        else:
+            logging.info("Less than one, so switching 's' to 900.")
+            s = 900
+    r = apiSwitchValue(obj, s)
+    return r
+
+
+@app.route('/switch/<obj>/<int:val>', methods=['POST'])
+def apiSwitchValue(obj, val):
+    # Switches the lights or fan either on or off, using the int 'val' as a
+    # boolean for lights and a time value for fan. In both cases, a value of 0
+    # switches the relevant object off.
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    logging.info("api.apiSwitchValue Function: obj: {}, val: {}".format(obj, val))
+    s = bool(val)
+    if obj == "lights":
+        global lightsOn
+        switchLights(s)
+    if obj == "fan":
+        switchFan(s, val)
+    data = standardData()
+    data["message"] = "{} switched {}.".format(obj.title(), status(val).title())
+    logging.info(data["message"])
+    return jsonify(data)
+
+
+@app.route('/stats', methods=['GET'])
+def apiShowStatsDefault():
+    # Shows some hardware stats on the screen for 10s
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    r = apiShowStats(10)
+    return r
+
+
+@app.route('/stats/<int:sleep>', methods=['POST'])
+def apiShowStats(sleep):
+    # Shows some hardware stats on the screen for the given time.
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    logging.info("api.apiShowStatus Function: sleep: {}".format(sleep))
+    global PiD
+    global pause
+    if (type(sleep) != int):
+        data = {
+            "success": False,
+            "message": "Sleep time not an integer."
+        }
+        return jsonify(data)
+    else:
+        pause = (sleep + 1)
+        while pause > 0:
+            PiD.showStats()
+            time.sleep(0.5)
+    data = {
+        "success": True,
+        "message": "Finished displaying data."
+    }
+    return jsonify(data)
+
+
+@app.route('/stats/vars', methods=['GET'])
+def apiShowVars():
+    # Shows the value of the standard variables on the screen for 5 seconds.
+    logging.info("Request URL: {} | User Agent: {} | Host: {}".format(request.url,
+                                                                      request.user_agent,
+                                                                      request.host))
+    logging.info("api.apiShowVars Function")
+    global PiD
+    global pause
+    pause = 6
+    while pause > 0:
+        data = standardData()
+        del data["message"]
+        del data["success"]
+        del data["pause"]
+        data["pin4"] = PiIO.readPin(4)
+        data["pin17"] = PiIO.readPin(17)
+        logging.info("apiShowVars Var Data: {}".format(data))
+        PiD.showVars(data)
+        time.sleep(1)
+    data = standardData()
+    data.update({
+        "success": True,
+        "message": "Finished displaying data."
+    })
+    return jsonify(data)
+
+
+# Non-route functions
 
 
 def status(s):
+    # Converts various data values into 'On' or 'Off' strings, for message formatting.
     y = [1, "1", True, "true", "on"]
     n = [0, "0", False, "false", "off"]
     if type(s) == str:
         s = s.lower()
+    if type(s) == int:
+        if s > 0:
+            s = True
+        else:
+            s = False
+    logging.info("api.status Function: s: {}".format(s))
     if s in y:
         return "On"
     if s in n:
@@ -96,93 +193,77 @@ def status(s):
     return "Unknown"
 
 
-@app.route('/', methods=['GET'])
-def apiIndex():
+def standardData():
+    # Returns a dict of the standard variables to return to users via JSONify
     global lightsOn
+    global fanTimer
+    global PiIO
+    global pause
     data = {
         "fanTimer": fanTimer,
         "lights": lightsOn,
         "success": True,
         "message": "",
-        "piconnected": pi3.connected
+        "piconnected": PiIO.pi.connected,
+        "pause": pause
     }
-    return jsonify(data)
+    logging.info("standardData returned: {}".format(str(data)))
+    return data
 
 
-@app.route('/add/<obj>/<val>', methods=['POST'])
-def apiAdd(obj, val):
+def switchFan(s, t=900):
+    # Adds and removes time from the fan timer, and actually switches the
+    # physical object on and off accordingly. 's' is a boolean value, 't'
+    # is only used if 's' is True.
     global fanTimer
-    global lightsOn
-    if obj == "fan":
-        fanTimer = fanTimer + int(val)
-        apiSwitch(obj, 1)
-        message = "{} added to the fan timer.".format(format_timespan(int(val)))
-    data = {
-        "fanTimer": fanTimer,
-        "lights": lightsOn,
-        "success": True,
-        "message": message,
-        "piconnected": pi3.connected
-    }
-    return jsonify(data)
-
-
-@app.route('/switch/<obj>/<int:val>', methods=['POST'])
-def apiSwitch(obj, val):
-    val = bool(val)
-    global lightsOn
-    global fanTimer
-    if obj == "lights":
-        switchLights(val)
-    if obj == "fan":
-        switchFan(val)
-    message = "{} switched {}.".format(obj.title(), status(val).title())
-    data = {
-        "fanTimer": fanTimer,
-        "lights": lightsOn,
-        "success": True,
-        "message": message,
-        "piconnected": pi3.connected
-    }
-    return jsonify(data)
-
-
-def switchFan(val):
-    global fanTimer
-    # Fan is assigned to GPIO pin 4
-    p = 4
-    pi3.set_mode(p, pigpio.OUTPUT)
-    if not val:
+    logging.info("api.switchFan Function: s: {}, t:{}".format(s, t))
+    if not s:
         fanTimer = 0
-    pi3.write(p, int(val))
+        PiD.writeFanTimer(0)
+    else:
+        fanTimer = fanTimer + t
+    logging.info("api.switchFan about to run PiIO.switchFan with argument s: {}".format(int(s)))
+    PiIO.switchFan(int(s))
 
 
-def switchLights(val):
+def switchLights(s):
+    # Physically switches the lights on and off.
+    logging.info("api.switchLights Function: s: {}".format(s))
     global lightsOn
-    # Fan is assigned to GPIO pin 4
-    p = 17
-    pi3.set_mode(p, pigpio.OUTPUT)
-    lightsOn = val
-    pi3.write(p, int(val))
+    lightsOn = s
+    PiIO.switchLight(int(s))
 
 
 def checker():
+    # The background function that keeps track of the fan timer
+    # and writes its value to the screen.
     global lightsOn
     global fanTimer
     global PiD
+    global pause
     sleep_c = 0
     while True:
-        if fanTimer > 0:
-            PiD.writeText("Fan Time: {}s".format(str(fanTimer)))
-            sleep_c = 0
-            fanTimer -= 1
-            if fanTimer == 0:
-                switchFan(False)
+        if pause > 0:
+            time.sleep(1)
+            pause -= 1
+            if pause < 0:
+                pause = 0
         else:
-            sleep_c += 1
-            if sleep_c == 5:
-                PiD.screenOff()
-        time.sleep(1)
+            if fanTimer > 0:
+                PiD.writeFanTimer(fanTimer)
+                sleep_c = 0
+                fanTimer -= 1
+                if fanTimer == 0:
+                    logging.info("api.checker switching off the fan!")
+                    switchFan(False)
+            else:
+                sleep_c += 1
+                if sleep_c == 10:
+                    PiD.screenOff()
+                if fanTimer < 0:
+                    fanTimer = 0
+            time.sleep(1)
+        PiIO.readPin(22)
 
 
 checkT = threading.Thread(group=None, target=checker)
--- api/api.wsgi
+++ api/api.wsgi
@@ -1,7 +1,23 @@
 #!/usr/bin/python
 import sys
+import os
 import logging
-logging.basicConfig(stream=sys.stderr)
+import logging.handlers
+# logging.basicConfig(stream=sys.stderr)
+basedir = os.path.abspath(os.path.dirname(__file__))
+i_log = os.path.join(basedir, "logs", "api_info.log")
+rh = logging.handlers.RotatingFileHandler(
+    filename=i_log,
+    mode='a',
+    maxBytes=(5 * 1024 * 1024),
+    backupCount=2,
+    encoding=None,
+    delay=0)
+logging.basicConfig(handlers=[rh],
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s - (%(lineno)d): %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+
 
 sys.path.insert(0, "/var/www/html/api")
 from api import app as application
